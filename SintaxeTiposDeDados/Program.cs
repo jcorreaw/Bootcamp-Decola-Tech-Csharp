07761324817559e7908189a9389b5db514d13330
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-01 18:43:22 -0300

    Tipos de Variaveis

--- SintaxeTiposDeDados/Program.cs
+++ SintaxeTiposDeDados/Program.cs
@@ -5,3 +5,22 @@ Pessoa pessoa1 = new Pessoa();
 pessoa1.Nome = "João";
 pessoa1.Idade = 22;
 pessoa1.Apresentar();
+
+string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da variavel altura: " + altura);
+Console.WriteLine("Valor da variavel preço: " + preco);
+Console.WriteLine("Valor da variavel condição: " + condicao);
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
