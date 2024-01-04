0bbfac39ba47350e832bae8d5410b817c674d384
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-04 18:13:08 -0300

    Atribuicao

--- TiposDeOperadores/Program.cs
+++ TiposDeOperadores/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Atribuição
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+
