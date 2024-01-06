b215425258ab116c4d280a4df52c2ca1d28d8790
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-06 10:54:10 -0300

    Convertendo variaveis

--- TiposDeOperadores/Program.cs
+++ TiposDeOperadores/Program.cs
@@ -15,3 +15,10 @@ Console.WriteLine(c);
 
 c *= 2;
 Console.WriteLine(c);
+
+//Convertendo Variaveis
+int converte1 = Convert.ToInt32("2");
+Console.WriteLine(converte1);
+
+int converte2 = int.Parse("2");
+Console.WriteLine(converte2);
