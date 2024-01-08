cae9a526657ee86e43c8cd59a94864d9ee17269d
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-08 18:13:34 -0300

    Operador condicional IF

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada!");
+}
+else
+{
+    Console.WriteLine("Desculpe, Não tempos a quantidade desejada em estoque.");
+}
--- Operadores/obj/Debug/net8.0/Operadores.AssemblyInfo.cs
+++ Operadores/obj/Debug/net8.0/Operadores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operadores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d46ad60bdc9d57ccb6c73da46aaa10f346a801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed440a4b8e4d4bbae0659b10e65752adbffe9358")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operadores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operadores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
