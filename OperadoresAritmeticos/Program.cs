41dcfa338a99b2ebb261beff7ef1e14c295d840c
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-10 18:11:54 -0300

    Funções Trigonométricas

--- OperadoresAritmeticos/Program.cs
+++ OperadoresAritmeticos/Program.cs
@@ -7,3 +7,6 @@ calc.Subtrair(10, 50);
 calc.Multiplicar(15, 45);
 calc.Dividir(2, 2);
 calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
--- OperadoresAritmeticos/obj/Debug/net8.0/OperadoresAritmeticos.AssemblyInfo.cs
+++ OperadoresAritmeticos/obj/Debug/net8.0/OperadoresAritmeticos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f14235c8e6bedf607b0a0efbaf4609083e1dda5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb85fb5b614c3eb7ad401684cfa475c7d4e26d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
