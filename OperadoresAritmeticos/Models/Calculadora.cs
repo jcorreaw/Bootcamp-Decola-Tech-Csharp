4f14235c8e6bedf607b0a0efbaf4609083e1dda5
Joao Carlos <juninhocorrea2014@gmail.com>
2024-01-10 18:02:20 -0300

    Calculadora

---
+++ OperadoresAritmeticos/Models/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿namespace OperadoresAritmeticos.Models;
+
+public class Calculadora
+{
+    public void Somar(int x, int y)
+    {
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+    
+    public void Subtrair(int x, int y)
+    {
+        Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+    
+    public void Multiplicar(int x, int y)
+    {
+        Console.WriteLine($"{x} x {y} = {x * y}");
+    }
+
+    public void Dividir(int x, int y)
+    {
+        Console.WriteLine($"{x} / {y} = {x / y}");
+    }
+}
